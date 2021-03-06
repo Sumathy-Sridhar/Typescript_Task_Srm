9d4b96669867068377f444c0cced73d18ccd233a
Sumathy-Sridhar <79184088+Sumathy-Sridhar@users.noreply.github.com>
2021-03-06 18:53:37 +0530

    Add files via upload

---
+++ SRM_Training/ReadInput.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+var customer = ["Brown", 123, "Bread", "Brown@gmail.com", 968745123, new Date()];
+console.log("Email Id of Customer : " + customer[3]);
+console.log("Customer Details : " + customer);
+console.log("Length of Tuple :" + customer.length);
+customer.push("Cony", 124, "Milk", "Cony@yahoo.com", 98745613112, new Date());
+console.log("Length of the Tuple : " + customer.length);
+console.log("Customer Details : ");
+for (var i = 0; i < customer.length; i++) {
+    console.log(customer[i]);
+}
+customer.pop();
+console.log("Length of Tuple After Pop Operation: " + customer.length);
+for (var i = 0; i < customer.length; i++) {
+    if (customer[i] == 'Cony') {
+        console.log("Cony Found at Index: " + i);
+    }
+}
